67008f9c003a738b880188c5664f770336620981
Martina Zambrano <mzambrano@ignetworks.com>
2023-04-18 21:18:48 -0300

    comienzo sprint 5

--- scripts/app.js
+++ scripts/app.js
@@ -11,11 +11,18 @@ const linkInput = document.getElementById("project-info__link");
 const video = document.getElementById("project-video");
 
 function loadYouTubeVideo(){
-    // https://www.youtube.com/watch?v=OWKXEJN67FE
-    // https://www.youtube.com/embed/9N9opF-PK5k
-    // https://www.youtube.com/watch?v=9N9opF-PK5k
-   const url= linkInput.value;
-   video.setAttribute('src',url);         
+  // https://www.youtube.com/watch?v=OWKXEJN67FE
+  // https://www.youtube.com/embed/9N9opF-PK5k
+  // https://www.youtube.com/watch?v=9N9opF-PK5k
+  // const youtubeUrl = 'https://www.youtube.com/watch?v=OWKXEJN67FE&t=4'
+  const url = linkInput.value;
+  const queryString = url.slice(url.indexOf('?'))
+  const params = new URLSearchParams(queryString)
+  const videoId = params.get('v')
+  if (videoId != null) {
+    const youtubeEmbedUrl = `https://www.youtube.com/embed/${video}`
+    video.setAttribute('src', youtubeEmbedUrl);
   }
+}
 function saveProject(){}
 
